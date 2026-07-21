168b2f88b538f8ed29d72ed7393052ff56aaf8ad
ΗΛΙΑΣ-ΜΙΧΑΗΛ ΚΟΥΤΣΙΑΣ <eliaskoutsias79@gmail.com>
2026-07-21 21:52:07 +0300

    Import MobileDashboard component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import MobileDashboard from "./mobile/MobileDashboard";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
