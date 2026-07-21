b10d27a99df7fef74cfde91a2fe456917f83c6c4
ΗΛΙΑΣ-ΜΙΧΑΗΛ ΚΟΥΤΣΙΑΣ <eliaskoutsias79@gmail.com>
2026-07-21 21:51:36 +0300

    Add MobileDashboard component with styling

--- src/mobile/mobileDashboard.jsx
+++ src/mobile/mobileDashboard.jsx
@@ -1,1 +1,18 @@
-
+export default function MobileDashboard() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#0f172a",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+        fontWeight: "700",
+      }}
+    >
+      📱 Mobile Version
+    </div>
+  );
+}
